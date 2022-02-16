d7504153edde03f24065220110cf2cc3c32a7af6
Joao Vitor Pereira da Silva <joaosilva@cijunsp.local>
2022-02-16 12:37:43 -0300

    Adicição exercicio 10 e 11

---
+++ Exercicio_10/Program.cs
@@ -0,0 +1,34 @@
+﻿    using System;
+
+    namespace teste
+    {
+        class Program
+        {
+            static void Main(string[] args){
+            
+                int num = 0; 
+                int fatorial, cont;
+                fatorial =1;
+                
+                Console.WriteLine("Digite um número qualquer maior que zero:");
+                num = int.Parse(Console.ReadLine());
+                    
+                    for(cont =num; cont >=1; cont --){
+
+                        fatorial *= cont;
+                    }
+                Console.WriteLine("O fatorial é: " +fatorial);
+
+            }  
+        }
+
+    }
+            
+
+
+                
+                
+            
+        
+
+    
